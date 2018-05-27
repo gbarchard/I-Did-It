bb2bfeab7a9cd919fc4c1875d5d576b660b3ff4b
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-05-27 10:42:57 -0400

    fixed class name

--- src/GoogleSignIn.js
+++ src/GoogleSignIn.js
@@ -4,7 +4,7 @@ export class GoogleSignIn extends Component {
 
     render() {
         return (
-            <div class="g-signin2" data-onsuccess="onSignIn"></div>
+            <div className="g-signin2" data-onsuccess="onSignIn"></div>
         );
     }
 }
