9e3a8ede123bfb0d9d5f66ff24c5bdd7dc444c54
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-05-27 10:50:20 -0400

    Google Sign-In log data

--- src/GoogleSignIn.js
+++ src/GoogleSignIn.js
@@ -2,9 +2,30 @@ import React, { Component } from 'react';
 
 export class GoogleSignIn extends Component {
 
+    componentDidMount() {
+      function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      };
+
+      
+    }
+
     render() {
         return (
             <div className="g-signin2" data-onsuccess="onSignIn"></div>
+
+            
         );
     }
 }
