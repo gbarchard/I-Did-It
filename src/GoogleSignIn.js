441fa06d076f8783b0f038f886f2b9ed2c645576
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-05-27 11:50:30 -0400

    Trying to get sign-in button looking good.

--- src/GoogleSignIn.js
+++ src/GoogleSignIn.js
@@ -9,12 +9,15 @@ export class GoogleSignIn extends Component {
 
 	render() {
 		return (
+			<div>
 			<GoogleLogin className="g-signin2"
 				clientId="1055110234972-c4j11hps4tctcj0cvt8aonjk6l121j1m.apps.googleusercontent.com"
 				buttonText="Login"
 				onSuccess={this.responseGoogle}
 				onFailure={this.responseGoogle}
 			/>
+			<div className="g-signin2" data-onsuccess="onSignIn"></div>
+			</div>
 		);
 	}
 }
