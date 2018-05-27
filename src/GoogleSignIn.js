31b65b6fac5fd0775434fdc847030724d7ce2bae
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-05-27 11:23:16 -0400

    Trying to get formatting right for google sign in

--- src/GoogleSignIn.js
+++ src/GoogleSignIn.js
@@ -3,22 +3,18 @@ import { GoogleLogin } from 'react-google-login';
 
 export class GoogleSignIn extends Component {
 
-    componentDidMount() {
-    
-    }
+	responseGoogle = (response) => {
+		console.log(response);
+	}
 
-    responseGoogle = (response) => {
-        console.log(response);
-    }
-
-    render() {
-        return (
-            <GoogleLogin
-                clientId="1055110234972-c4j11hps4tctcj0cvt8aonjk6l121j1m.apps.googleusercontent.com"
-                buttonText="Login"
-                onSuccess={this.responseGoogle}
-                onFailure={this.responseGoogle}
-            />
-        );
-    }
+	render() {
+		return (
+			<GoogleLogin className="g-signin2"
+				clientId="1055110234972-c4j11hps4tctcj0cvt8aonjk6l121j1m.apps.googleusercontent.com"
+				buttonText="Login"
+				onSuccess={this.responseGoogle}
+				onFailure={this.responseGoogle}
+			/>
+		);
+	}
 }
