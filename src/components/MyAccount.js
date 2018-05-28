53a2d07f85ea29fd9de1fadf45f7bb3a514051bf
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-05-28 12:18:37 -0400

    fixed image size

--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -4,7 +4,7 @@ export class MyAccount extends Component {
 
     render() {
         return (
-            <img src={this.props.image} alt="My Account" height="42" width="42"></img>
+            <img src={this.props.image} alt="My Account"></img>
         );
     }
 }
