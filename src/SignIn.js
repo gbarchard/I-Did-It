5713e193a378c3142ae9c85a78cdaaa52014102f
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-05-27 20:09:27 -0400

    going for google again

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import { GoogleLogin } from 'react-google-login';
 
 export class SignIn extends Component {
 
+    responseGoogleSuccess = (response) => {
+        console.log(response);
+        this.props.setSignedInValue(true)
+
+    }
+
+    responseGoogleFailure = (response) => {
+        console.log(response);
+        this.props.setSignedInValue(false)
+
+    }
+      
     SignIn = () => {
         this.props.setSignedInValue(true)
     }
 
     render() {
         return (
-            <button onClick={this.SignIn}>Sign In</button>
+            //<button onClick={this.SignIn}>Sign In</button>
+            <GoogleLogin
+                clientId="1055110234972-c4j11hps4tctcj0cvt8aonjk6l121j1m.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={this.responseGoogleSuccess}
+                onFailure={this.responseGoogleFailure}
+            />
         );
     }
 }
