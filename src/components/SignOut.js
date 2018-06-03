e419ca9f62a19f9a56f1dc0ab13f8e33c42ff899
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-06-03 07:54:16 -0400

    changed font size to be vw

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 const SignOutText = styled.p`
     text-decoration: underline;
     cursor: pointer;
+    font-size: 4vw;
 `
 
 export class SignOut extends Component {
