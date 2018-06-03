78281b5c3952d432b8efd59312b03b1c27a0ffd8
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-06-03 08:03:02 -0400

    centered signup button

--- src/views/Unauthenticated.js
+++ src/views/Unauthenticated.js
@@ -3,11 +3,31 @@ import styled from "styled-components";
 
 import { SignIn } from '../components/SignIn';
 
-export class Unauthenticated extends Component {
+const View = styled.div`
+	display: grid;  
+	grid-template-columns: 1fr;
+	grid-template-rows: 98vh;
+`;
 
+const SignInPosition = styled.div`
+	grid-column-start: 1;
+	grid-column-end: 2;
+	grid-row-start: 1;
+	grid-row-end: 2;
+	display: flex;
+	align-self: center;
+  align-items: center;
+  justify-content: center;
+`;
+
+export class Unauthenticated extends Component {
 	render() {
 		return (
-			<SignIn setSignedInValue={this.props.setSignedInValue}/>
+			<View>
+				<SignInPosition>
+					<SignIn setSignedInValue={this.props.setSignedInValue}/>
+				</SignInPosition>	
+			</View>
 		);
 	}
 }
