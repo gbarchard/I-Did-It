c09de8e22304c2e26b2d4a8e34cecdb7effe3918
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-06-03 19:57:23 -0400

    how big can I Did It be

--- src/components/IDidIt.js
+++ src/components/IDidIt.js
@@ -10,7 +10,7 @@ const IDidItButton = styled.button`
     border: 2px solid #B3A369;
     width: 80vw;
     height: 80vw;
-    font-size: 20vmin;
+    font-size: 10vmin;
 
     &:active {
         background-color: white;
