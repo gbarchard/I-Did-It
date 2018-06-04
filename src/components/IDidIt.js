34af8316eadfd58d3a6d673252d67188ad577255
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-06-03 20:00:19 -0400

    15 works, 20 doesn;t, trying 18

--- src/components/IDidIt.js
+++ src/components/IDidIt.js
@@ -10,7 +10,7 @@ const IDidItButton = styled.button`
     border: 2px solid #B3A369;
     width: 80vw;
     height: 80vw;
-    font-size: 15vmin;
+    font-size: 18vmin;
 
     &:active {
         background-color: white;
