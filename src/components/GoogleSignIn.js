968e74a851c99dd05d1eea1da100d09a34b8e0b2
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2019-01-27 18:31:57 -0500

    kind of working

--- src/components/GoogleSignIn.js
+++ src/components/GoogleSignIn.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { request } from 'graphql-request';
 
 export class GoogleSignIn extends Component {
 	constructor(props) {
@@ -15,7 +16,30 @@ export class GoogleSignIn extends Component {
 	}
 
 	onSignIn(response) {
-		this.props.setSignedInValue(true, response)
+		const mutation = `mutation {
+			addUser(
+				firstName: "${response.w3.ofa}",
+				lastName: "${response.w3.wea}", 
+				email: "${response.w3.U3}",
+				token: "${response.Zi.id_token}",
+				image: "${response.w3.Paa}"
+			)` +
+			`{
+				id
+				firstName
+				lastName
+				email
+				token
+				image
+				created
+			}
+		}`
+		console.log(mutation)
+		request('http://localhost:3000/graphql', mutation).then(data => {
+			console.log(data)
+			this.props.setSignedInValue(true, response)
+		})
+		
 	}
 
 	render() {
